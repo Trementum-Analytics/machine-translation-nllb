fbabddaff109b2616c40de0209f24b6a02d595a5
Ihor Lukianov <lukianov.igor@hotmail.com>
2023-07-07 19:39:30 +0700

    completed openai moderation

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Detect languages for texts with fb model for flores dataset format
 2. Perform translation and store it in spaces bucket
 3. OpenAi moderation - https://platform.openai.com/docs/guides/moderation
+
--- openai_moderation/src/main_moderation.py
+++ openai_moderation/src/main_moderation.py
@@ -1,8 +1,10 @@
 import openai
 import os
 from dotenv import load_dotenv
+import json
 from db_connection import DbConnection
 
+BATCH_SIZE = 1
 
 # init openai api
 load_dotenv()
@@ -23,13 +25,50 @@ query = '''
     SELECT _id, text_original
     FROM test_all_platforms.posts
     WHERE text_original IS NOT NULL
-        AND lang_detect IS NOT NULL
-'''    
+    LIMIT 5
+'''
 
-response = openai.Moderation.create(
-    input="Sample text goes here"
-)
+# create cursor for getting data from the database
+cur = Connection.conn.cursor()
+cur.execute(query)
 
-output = response["results"][0]
+# load first batch of rows - if nothing, it just goes to the end of script
+rows = cur.fetchmany(BATCH_SIZE)
 
-print(output)
+i = 0
+while rows:
+    data = []
+    for row in rows:
+        text = row[1]
+        
+        response = openai.Moderation.create(
+            input=text
+        )
+
+        output = response["results"][0]
+        categories = json.loads(str(output['categories']))
+        category_scores = json.loads(str(output['category_scores']))
+        data.append({'id': int(row[0]),
+                     'flagged': output['flagged'],
+                     'categories': json.dumps(categories),
+                     'category_scores': json.dumps(category_scores)})
+    
+    update_statement = '''
+    UPDATE test_all_platforms.posts
+    SET flagged = %(flagged)s,
+        categories = %(categories)s
+    WHERE _id = %(id)s'''
+
+    Connection.updated_in_batches(update_statement, data)
+
+    i += 1
+    print(f'Done {i*BATCH_SIZE} posts')
+
+    # load next batch of rows
+    rows = cur.fetchmany(BATCH_SIZE)
+
+Connection.commit()
+
+# close connection after everything is done
+cur.close()
+Connection.close_connection()
