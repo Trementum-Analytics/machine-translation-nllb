a2ebdb0e165a58cf549c8bad396fb667c3da52f6
Ihor Lukianov <lukianov.igor@hotmail.com>
2023-07-10 10:33:16 +0700

    added connection to database for translation

--- translation/src/main_translation.py
+++ translation/src/main_translation.py
@@ -1,6 +1,35 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 from torch import cuda
+from db_connection import DbConnection
 import re
+
+BATCH_SIZE = 1000
+
+# start database connection
+try:
+    # initialize connection to database
+    Connection = DbConnection()
+    Connection.connect()
+    
+    print('Connection successful')
+except Exception as error:
+    print('Connection error: ', error)
+
+# load data for translation
+query = '''
+    SELECT _id, text_original
+    FROM test_all_platforms.posts
+    WHERE text_original IS NOT NULL
+    AND lang = 'es'
+    LIMIT 1
+'''
+
+# create cursor for getting data from the database
+cur = Connection.conn.cursor()
+cur.execute(query)
+
+# load first batch of rows - if nothing, it just goes to the end of script
+rows = cur.fetchmany(BATCH_SIZE)
 
 device = 0 if cuda.is_available() else -1
 
@@ -10,7 +39,7 @@ CKPT = "facebook/nllb-200-distilled-600M"
 model = AutoModelForSeq2SeqLM.from_pretrained(CKPT)
 tokenizer = AutoTokenizer.from_pretrained(CKPT)
 
-src_lang = 'ita_Latn'
+src_lang = 'spa_Latn'
 tgt_lang = 'eng_Latn'
 max_length = 400
 
@@ -22,11 +51,12 @@ translation_pipeline = pipeline(TASK,
                                 max_length=max_length,
                                 device=device)
 
-text = '@AgainCarlakak Ma che cazz sta dicendo? Perché hanno la  demenza è devono morire!! ma sto faccia di merda lo dice strac…'
-delimiters = ['? ', '! ', '. ']
-
 # Create a regular expression pattern with the values as separators
+delimiters = ['? ', '! ', '. ']
 pattern = '|'.join(map(re.escape, delimiters))
+
+for row in rows:
+    text = row[1]
 
 # Split the sentence using the pattern and keep the separators
 result = re.split(f'({pattern})', text)
@@ -37,3 +67,6 @@ merged_result = [result[i] + result[i+1][0]
 
 res = ' '.join([translation_pipeline(res)[0]['translation_text']
                for res in merged_result])
+
+print(text)
+print(res)
